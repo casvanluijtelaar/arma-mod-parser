ac704e9882db2dbdbcc48fe838d49ff2a45e448a
Cas van Luijtelaar <cas.luijtelaar@umain.com>
2023-04-04 18:23:42 +0200

    fix parsing

--- bin/modparser.dart
+++ bin/modparser.dart
@@ -2,8 +2,10 @@ import 'dart:io';
 
 import 'package:args/args.dart';
 import 'package:html/parser.dart';
+import 'package:html_unescape/html_unescape.dart';
 
 void main(List<String> arguments) async {
+  final unescape = HtmlUnescape();
   final parser = ArgParser()..addOption("file", abbr: "f");
 
   final results = parser.parse(arguments);
@@ -22,12 +24,17 @@ void main(List<String> arguments) async {
     print("Mods found: ${mods.length}");
     print("");
 
-    final names = mods.map((mod) => mod.innerHtml);
+    final allowedCharacterRegex = RegExp("[^a-zA-Z0-9_. -]+");
+
+    final names = mods.map((mod) {
+      final name = unescape.convert(mod.innerHtml);
+      return name.replaceAll(allowedCharacterRegex, "");
+    });
+
     final formattedNames = names.map((name) => "@$name;");
 
     print(formattedNames.join(""));
     print("");
-    
   } catch (e) {
     print("\x1B[2J\x1B[0;0H");
     print("Something went wrong, is this a valid Arma mod list?");
